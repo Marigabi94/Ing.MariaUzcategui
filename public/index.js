7fcab10ec99d7ff774cdd2854fe8335281d4c899
Marigabi94 <marigabi940@gmail.com>
2021-01-23 15:52:36 -0400

    first commit

---
+++ public/index.js
@@ -0,0 +1,70 @@
+// TODO: CAMBIO DE VISTA AL SELECCIONAR UNA OPCION DEL NAVBAR
+
+function ClickNav(idButton) {
+  
+  var perfil = document.getElementById('perfil');
+  var experiencia = document.getElementById('experiencia'); 
+  var educacion = document.getElementById('educacion');
+  var contactame = document.getElementById('contactame');
+  var Logo1 = document.getElementById('LogoSeccion-1');
+  var Logo2 = document.getElementById('LogoSeccion-2');
+  var opcion1 = document.getElementById('opcion1');
+  var opcion2 = document.getElementById('opcion2');
+  var opcion3 = document.getElementById('opcion3');
+  var opcion4 = document.getElementById('opcion4');
+
+    switch (idButton) {
+        case 1:
+            perfil.style.display = 'block';
+            Logo2.style.display = 'none';
+            Logo1.style.display = 'grid';
+            experiencia.style.display = 'none';
+            educacion.style.display= 'none';
+            contactame.style.display= 'none';
+            opcion1.style.color = '#41aea9';
+            opcion2.style.color = '#e8ffff';
+            opcion3.style.color = '#e8ffff';
+            opcion4.style.color = '#e8ffff';
+            break;
+        case 2:
+          perfil.style.display = 'none';
+          Logo1.style.display = 'none';
+          Logo2.style.display = 'block';
+          experiencia.style.display = 'flex';
+          educacion.style.display= 'none';
+          contactame.style.display= 'none';
+          opcion1.style.color = '#e8ffff';
+          opcion2.style.color = '#41aea9';
+          opcion3.style.color = '#e8ffff';
+          opcion4.style.color = '#e8ffff';
+          experiencia.scrollTop= 0;
+          break;
+        case 3:
+          perfil.style.display = 'none';
+          Logo1.style.display = 'none';
+          Logo2.style.display = 'block';
+          experiencia.style.display = 'none';
+          educacion.style.display= 'grid';
+          contactame.style.display= 'none';
+          opcion1.style.color = '#e8ffff';
+          opcion2.style.color = '#e8ffff';
+          opcion3.style.color = '#41aea9';
+          opcion4.style.color = '#e8ffff';
+          break;
+        case 4:
+          perfil.style.display = 'none';
+          Logo1.style.display = 'grid';
+          Logo2.style.display = 'none';
+          experiencia.style.display = 'none';
+          educacion.style.display= 'none';
+          contactame.style.display= 'flow-root';
+          opcion1.style.color = '#e8ffff';
+          opcion2.style.color = '#e8ffff';
+          opcion3.style.color = '#e8ffff';
+          opcion4.style.color = '#41aea9';
+          break;
+       
+      }
+
+}
+
